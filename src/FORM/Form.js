568e06c3e3236139445edcba5abdc246f8e80cd4
Md akter hosen <mdaktarhossen20@gmail.com>
2022-01-28 22:50:24 +0600

    form controle component completed

--- src/FORM/Form.js
+++ src/FORM/Form.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form() {
+  const [name, setName]=useState("")
+  const [email, setEmail]=useState("")
+  const [password, setPassword]=useState("")
+
   const handleName=(x)=>{
-  console.log(x.target.value);
+    setName(x.target.value)
   }
   const handleEmail=(x)=>{
-  console.log(x.target.value);
+    setEmail(x.target.value)
   }
   const handlePassword=(x)=>{
-  console.log(x.target.value);
+    setPassword(x.target.value)
   }
   const handleSubmit=(x)=>{
   console.log("Form is submited");
+  console.log([`name is: ${name}, password is: ${password}, Email is: ${email}`])
   x.preventDefault()
   }
 
@@ -20,9 +25,9 @@ export default function Form() {
     <div>
       <form action="" onSubmit={handleSubmit}>
         <h1>Registration form</h1>
-        <h1>Name: <input type="text" onChange={handleName}/></h1>
-        <h1>Email: <input type="Email" onChange={handleEmail}/></h1>
-        <h1>Password: <input type="password" onChange={handlePassword}/></h1>
+        <h1>Name: <input type="text" value={name} onChange={handleName}/></h1>
+        <h1>Email: <input type="Email" value={email} onChange={handleEmail}/></h1>
+        <h1>Password: <input type="password" value={password} onChange={handlePassword}/></h1>
         <h1><button type='submit'>Registration</button></h1>
       </form>
     </div>
